b9b5213116d55e3cc71763c4bcff69bc149a9708
José Miguel Ramos <miguelyinyang@icloud.com>
2026-07-08 21:23:53 +0200

    Fix survey manager API flow

--- backend/app/services/survey_manager.py
+++ backend/app/services/survey_manager.py
@@ -1,14 +1,15 @@
 import requests
+import json
 
-BASE_URL = 'http://127.0.0.1:8000'
+BASE_URL = 'http://127.0.0.1:5002'
 
-def trigger_survey():
+def build_sample_survey():
     return {
   "survey_type": "running_plan",
   "answers": {
-    "goal": "run_10k",
+    "goal": "run_5k",
     "race_date": "2026-09-20",
-    "experience_level": "beginner_to_intermediate",
+    "experience_level": "intermediate",
     "current_weekly_distance_km": 15,
     "runs_per_week": 3,
     "preferred_days": ["Tuesday", "Thursday", "Sunday"],
@@ -22,14 +23,30 @@ def trigger_survey():
   }
 }
 
-def build_survey_package(survey: dict, user_id: str):
+def build_survey_package(survey: dict):
     return {
-        'user_id': user_id,
         'survey_type': survey['survey_type'],
-        'answers': survey
+        'answers': survey['answers'],
     }
 
-def save_survey(playload):
-    response = requests.post(f'{BASE_URL}/surveys/', json=playload)
+def save_survey(user_id, payload):
+    response = requests.post(f'{BASE_URL}/surveys/users/{user_id}', json=payload)
     response.raise_for_status()
     return response.json()
+
+
+def run(user_id):
+    survey = build_sample_survey()
+    payload = build_survey_package(survey)
+    saved_survey = save_survey(user_id, payload)
+    return saved_survey
+
+
+if __name__ == "__main__":
+    test_user_id = "328cae0c-b9fe-4d3e-ac20-7fc642b406e1"
+
+    result = run(test_user_id)
+
+    print(json.dumps(result, indent=4))
+
+# uvicorn app.main:app --reload --port 5002
